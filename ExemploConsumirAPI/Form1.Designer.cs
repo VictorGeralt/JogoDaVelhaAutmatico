46ad9d6c2a28e7a9318060381354be5263b083fa
victor geraldo <victor.vic2009@hotmail.com>
2022-04-14 04:09:41 -0300

    first commit

--- ExemploConsumirAPI/Form1.Designer.cs
+++ ExemploConsumirAPI/Form1.Designer.cs
@@ -32,23 +32,13 @@
             this.txtURL = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.cmbMarcador = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.nudLinha = new System.Windows.Forms.NumericUpDown();
-            this.nudColuna = new System.Windows.Forms.NumericUpDown();
-            this.label4 = new System.Windows.Forms.Label();
             this.btnJogar = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtRetorno = new System.Windows.Forms.TextBox();
-            this.btnTabuleiro = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.nudLinha)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudColuna)).BeginInit();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Location = new System.Drawing.Point(26, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 0;
@@ -56,16 +46,16 @@
             // 
             // txtURL
             // 
-            this.txtURL.Location = new System.Drawing.Point(50, 20);
+            this.txtURL.Location = new System.Drawing.Point(64, 33);
             this.txtURL.Name = "txtURL";
-            this.txtURL.Size = new System.Drawing.Size(421, 20);
+            this.txtURL.Size = new System.Drawing.Size(221, 20);
             this.txtURL.TabIndex = 1;
             this.txtURL.Text = "http://192.168.1.5:9000/";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Location = new System.Drawing.Point(26, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 2;
@@ -80,95 +70,28 @@
             this.cmbMarcador.Items.AddRange(new object[] {
             "X",
             "O"});
-            this.cmbMarcador.Location = new System.Drawing.Point(73, 51);
+            this.cmbMarcador.Location = new System.Drawing.Point(87, 64);
             this.cmbMarcador.Name = "cmbMarcador";
             this.cmbMarcador.Size = new System.Drawing.Size(121, 21);
             this.cmbMarcador.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 89);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Linha:";
-            // 
-            // nudLinha
-            // 
-            this.nudLinha.Location = new System.Drawing.Point(74, 87);
-            this.nudLinha.Name = "nudLinha";
-            this.nudLinha.Size = new System.Drawing.Size(120, 20);
-            this.nudLinha.TabIndex = 5;
-            // 
-            // nudColuna
-            // 
-            this.nudColuna.Location = new System.Drawing.Point(73, 113);
-            this.nudColuna.Name = "nudColuna";
-            this.nudColuna.Size = new System.Drawing.Size(120, 20);
-            this.nudColuna.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 115);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Coluna:";
+            this.cmbMarcador.SelectedIndexChanged += new System.EventHandler(this.cmbMarcador_SelectedIndexChanged);
             // 
             // btnJogar
             // 
-            this.btnJogar.Location = new System.Drawing.Point(27, 150);
+            this.btnJogar.Location = new System.Drawing.Point(250, 127);
             this.btnJogar.Name = "btnJogar";
-            this.btnJogar.Size = new System.Drawing.Size(444, 23);
+            this.btnJogar.Size = new System.Drawing.Size(66, 23);
             this.btnJogar.TabIndex = 8;
             this.btnJogar.Text = "Jogar";
             this.btnJogar.UseVisualStyleBackColor = true;
             this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.txtRetorno);
-            this.groupBox1.Location = new System.Drawing.Point(200, 46);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(271, 100);
-            this.groupBox1.TabIndex = 9;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Retorno da API";
-            // 
-            // txtRetorno
-            // 
-            this.txtRetorno.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtRetorno.Location = new System.Drawing.Point(3, 16);
-            this.txtRetorno.Multiline = true;
-            this.txtRetorno.Name = "txtRetorno";
-            this.txtRetorno.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtRetorno.Size = new System.Drawing.Size(265, 81);
-            this.txtRetorno.TabIndex = 0;
-            // 
-            // btnTabuleiro
-            // 
-            this.btnTabuleiro.Location = new System.Drawing.Point(27, 179);
-            this.btnTabuleiro.Name = "btnTabuleiro";
-            this.btnTabuleiro.Size = new System.Drawing.Size(107, 23);
-            this.btnTabuleiro.TabIndex = 10;
-            this.btnTabuleiro.Text = "Consultar tabuleiro";
-            this.btnTabuleiro.UseVisualStyleBackColor = true;
-            this.btnTabuleiro.Click += new System.EventHandler(this.btnTabuleiro_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 234);
-            this.Controls.Add(this.btnTabuleiro);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(342, 191);
             this.Controls.Add(this.btnJogar);
-            this.Controls.Add(this.nudColuna);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.nudLinha);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.cmbMarcador);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtURL);
@@ -177,10 +100,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Exemplo Consumir API";
-            ((System.ComponentModel.ISupportInitialize)(this.nudLinha)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudColuna)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,14 +111,7 @@
         private System.Windows.Forms.TextBox txtURL;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cmbMarcador;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown nudLinha;
-        private System.Windows.Forms.NumericUpDown nudColuna;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnJogar;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox txtRetorno;
-        private System.Windows.Forms.Button btnTabuleiro;
     }
 }
 
--- ExemploConsumirAPI/Form1.cs
+++ ExemploConsumirAPI/Form1.cs
@@ -16,26 +16,317 @@ namespace ExemploConsumirAPI
         private void btnJogar_Click(object sender, EventArgs e)
         {
             try
-            {
+            { 
                 //URL Base de acesso a API http://localhost:9000/
                 string baseAddress = txtURL.Text;
 
                 //Instancia um objeto de requisição HTTP
                 HttpClient client = new HttpClient();
 
-                //Realiza uma requisição GET a API  http://localhost:9000/api/jogo?marcador=X&linha=0&coluna=0
-                var response = client.GetAsync(baseAddress + "api/jogo?marcador=" + cmbMarcador.Text 
-                    + "&linha=" + Convert.ToInt32(nudLinha.Value)  
-                    + "&coluna=" + Convert.ToInt32(nudColuna.Value)).Result;
+                //Realiza uma requisição GET a API  http://localhost:9000/api/jogo
+                var leitura = client.GetAsync(baseAddress + "api/jogo").Result;
 
                 //Pega o resultado da requisição
-                var resultado = response.Content.ReadAsStringAsync().Result.ToString();
-
-                //Exibe o retorno em uma caixa de texto
-                txtRetorno.Text = resultado;
+                var resultado = leitura.Content.ReadAsStringAsync().Result.ToString();
 
                 //Converte o JSON em uma matris String
                 string[][] tabuleiro = JsonSerializer.Deserialize<string[][]>(resultado);
+
+                int cord1=5, cord2=5;
+
+
+                string antiMarcador;
+
+
+                if(cmbMarcador.Text == "X")
+                {
+                    antiMarcador = "O";
+                }
+                else { antiMarcador = "X"; }
+
+                int derrota = 0, vitoria = 0,cordV1=5,cordV2=5;
+
+
+                //jogo como X
+                if (cmbMarcador.Text == "X" && tabuleiro[0][0] == "")
+                {
+                    cord1 = 0;
+                    cord2 = 0;
+                }else if (cmbMarcador.Text == "X" && tabuleiro[2][2] == "")
+                {
+                    cord1 = 2;
+                    cord2 = 2;
+                }
+                else if (cmbMarcador.Text == "X" && tabuleiro[2][2] == "O" && tabuleiro[0][2] == "")
+                {
+                    cord1 = 0;
+                    cord2 = 2;
+                }
+                else if (cmbMarcador.Text == "X" && (tabuleiro[2][2] == "O" && tabuleiro[0][2] == "X"))
+                {
+                    cord1 = 2;
+                    cord2 = 0;
+                }
+
+                //jogo como O
+
+                if (cmbMarcador.Text == "O" && (tabuleiro[0][0] == "X" || tabuleiro[0][2] == "X"))
+                {
+                    cord1 = 0;
+                    cord2 = 1;
+                }else if(cmbMarcador.Text == "O" && (tabuleiro[1][1]=="X"))
+                {
+                    cord1 = 1;
+                    cord2 = 0;
+                }else if (cmbMarcador.Text == "O" && (tabuleiro[1][0] == "O")) {
+                    cord1 = 2;
+                    cord2 = 2;
+                }
+
+                if (cmbMarcador.Text == "O" && (tabuleiro[2][0] == "X" || tabuleiro[2][2] == "X"))
+                {
+                    cord1 = 2;
+                    cord2 = 1;
+                }
+                else if (cmbMarcador.Text == "O" && (tabuleiro[1][1] == "X"))
+                {
+                    cord1 = 1;
+                    cord2 = 0;
+                }
+                else if (cmbMarcador.Text == "O" && (tabuleiro[1][0] == "O"))
+                {
+                    cord1 = 2;
+                    cord2 = 2;
+                }
+
+
+
+                //verificação das linhas
+
+                for (int i = 0; i <=2; i++)
+                {
+                    for (int j = 0; j <=2; j++)
+                    {
+                        if (tabuleiro[i][j] == cmbMarcador.Text)
+                        {
+                            vitoria++;
+                        }
+                        if (tabuleiro[i][j] == antiMarcador)
+                        {
+                            derrota++;
+                        }
+                    }
+                    if (((vitoria == 2) && (derrota == 1)) || (derrota == 2) && (vitoria == 1))
+                    {
+                    }
+                    else if (derrota == 2)
+                    {
+                        for (int l = 0; l <=2 ; l++)
+                        {
+                            if (tabuleiro[i][l] == "")
+                            {
+                                cord1 = i;
+                                cord2 = l;
+                            }
+                        }
+                    }
+                    else if (vitoria == 2)
+                    {
+                        for (int l = 0; l <= 2; l++)
+                        {
+                            if (tabuleiro[i][l] == "")
+                            {
+                                cordV1 = i;
+                                cordV2 = l;
+                            }
+                        }
+                    }
+                    derrota = 0;
+                    vitoria = 0;
+                }
+
+                //verificação colunas
+
+                for (int j = 0; j <3 ; j++)
+                {
+                    for (int i = 0; i <3 ; i++)
+                    {
+                        if (tabuleiro[i][j] == cmbMarcador.Text)
+                        {
+                            vitoria++;
+                        }
+                        if (tabuleiro[i][j] == antiMarcador)
+                        {
+                            derrota++;
+                        }
+                    }
+                    if (((vitoria == 2) && (derrota == 1)) || (derrota == 2) && (vitoria == 1))
+                    {
+                    }
+                    else if (derrota == 2)
+                    {
+                        for (int l = 0; l <3; l++)
+                        {
+                            if (tabuleiro[l][j] == "")
+                            {
+                                cord1 = l;
+                                cord2 = j;
+                            }
+                    }
+                    }
+                    else if (vitoria == 2)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
+                            if (tabuleiro[l][j] == "")
+                            {
+                                cordV1 = l;
+                                cordV2 = j;
+                            }
+                        }
+                    }
+                    derrota = 0;
+                    vitoria = 0;
+                }
+
+                //analisando diagonais
+
+                for (int i = 0; i <3 ; i++)
+                {
+                    if (tabuleiro[i][i] == cmbMarcador.Text)
+                    {
+                        vitoria++;
+                    }
+                    if (tabuleiro[i][i] == antiMarcador)
+                    {
+                        derrota++;
+                    }
+                }
+                if (((vitoria == 2) && (derrota == 1)) || (derrota == 2) && (vitoria == 1))
+                {
+                }
+                else if (derrota == 2)
+                {
+                    for (int l = 0; l  <3 ; l++)
+                    {
+                        if (tabuleiro[l][l] == "")
+                        {
+                            cord1 = l;
+                            cord2 = l;
+                        }
+                    }
+                }
+
+                else if (vitoria == 2)
+                {
+                    for (int l = 0; l < 3; l++)
+                    {
+                        if (tabuleiro[l][l] == "")
+                        {
+                            cordV1 = l;
+                            cordV2 = l;
+                        }
+                    }
+                }
+                derrota = 0;
+                vitoria = 0;
+
+
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (tabuleiro[i][2-i] == cmbMarcador.Text)
+                    {
+                        vitoria++;
+                    }
+                    if (tabuleiro[i][2-i] == antiMarcador)
+                    {
+                        derrota++;
+                    }
+                }
+                if (((vitoria == 2) && (derrota == 1)) || (derrota == 2) && (vitoria == 1))
+                {
+                }
+
+                else if (derrota == 2)
+                {
+                    for (int l = 0; l < 3; l++)
+                    {
+                        if (tabuleiro[l][2 - l] == "")
+                        {
+                            cord1 = l;
+                            cord2 = 2-l;
+                        }
+                    }
+                }
+                else if (vitoria == 2)
+                {
+                    for (int l = 0; l <3; l++)
+                    {
+                        if (tabuleiro[l][2 - l] == "")
+                        {
+                            cordV1 = l;
+                            cordV2 = 2-l;
+                        }
+                    }
+                }
+                derrota = 0;
+                vitoria = 0;
+
+
+                if (cordV1!=5)
+                {
+                    cord1 = cordV1;
+                    cord2 = cordV2;
+                }
+
+                if (cord1 == 5 && tabuleiro[0][0]=="" || (tabuleiro[cord1][cord2]=="X" || (tabuleiro[cord1][cord2] == "O" ) && tabuleiro[0][0] == "" )) {
+                    cord1 = 0;
+                    cord2 = 0;
+                }else if (cord1 == 5 && tabuleiro[0][2] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[0][2] == ""))
+                {
+                    cord1 = 0;
+                    cord2 = 2;
+                }
+                else if (cord1 == 5 && tabuleiro[2][0] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[2][0] == ""))
+                {
+                    cord1 = 2;
+                    cord2 = 0;
+                }
+                else if (cord1 == 5 && tabuleiro[2][2] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[2][2] == ""))
+                {
+                    cord1 = 2;
+                    cord2 = 2;
+                }
+                else if (cord1 == 5 && tabuleiro[0][1] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[0][1] == ""))
+                {
+                    cord1 = 0;
+                    cord2 = 1;
+                }
+                else if (cord1 == 5 && tabuleiro[1][0] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[1][0] == ""))
+                {
+                    cord1 = 1;
+                    cord2 = 0;
+                }
+                else if (cord1 == 5 && tabuleiro[1][2] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[1][2] == ""))
+                {
+                    cord1 = 1;
+                    cord2 = 2;
+                }
+                else if (cord1 == 5 && tabuleiro[2][1] == "" || (tabuleiro[cord1][cord2] == "X" || (tabuleiro[cord1][cord2] == "O") && tabuleiro[2][1] == ""))
+                {
+                    cord1 = 2;
+                    cord2 = 1;
+                }
+
+
+
+                //Realiza uma requisição GET a API  http://localhost:9000/api/jogo?marcador=X&linha=0&coluna=0
+                var jogada = client.GetAsync(baseAddress + "api/jogo?marcador=" + cmbMarcador.Text 
+                    + "&linha=" + Convert.ToInt32(cord1)  
+                    + "&coluna=" + Convert.ToInt32(cord2)).Result;
+
+               
 
 
             }
@@ -47,30 +338,11 @@ namespace ExemploConsumirAPI
 
         private void btnTabuleiro_Click(object sender, EventArgs e)
         {
-            try
-            {
-                //URL Base de acesso a API http://localhost:9000/
-                string baseAddress = txtURL.Text;
+        }
 
-                //Instancia um objeto de requisição HTTP
-                HttpClient client = new HttpClient();
+        private void cmbMarcador_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
-                //Realiza uma requisição GET a API  http://localhost:9000/api/jogo
-                var response = client.GetAsync(baseAddress + "api/jogo").Result;
-
-                //Pega o resultado da requisição
-                var resultado = response.Content.ReadAsStringAsync().Result.ToString();
-
-                //Exibe o retorno em uma caixa de texto
-                txtRetorno.Text = resultado;
-
-                //Converte o JSON em uma matris String
-                string[][] tabuleiro = JsonSerializer.Deserialize<string[][]>(resultado);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
     }
 }
